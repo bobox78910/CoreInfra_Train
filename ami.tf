2e689be3f125c6bdbe8402328c6d501f02846253
bobox78910 <33033728+bobox78910@users.noreply.github.com>
2017-10-24 16:21:42 +0200

    Update ami.tf

--- ami.tf
+++ ami.tf
@@ -16,7 +16,10 @@ data "aws_ami" "ubuntu" {
 }
 
 
-resource "aws_security_group" "allow_all" {  name        = "allow_all"  description = "Allow all inbound traffic"  vpc_id = “xxxxx “
+resource "aws_security_group" "allow_all" {  
+  name        = "allow_all"  
+  description = "Allow all inbound traffic"  
+  vpc_id = “mainBD“
   ingress {    
     from_port   = 22    
     to_port     = 22    
