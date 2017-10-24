9e04d30f10e4ec0955be5c6da7444b003f960fc2
bobox78910 <33033728+bobox78910@users.noreply.github.com>
2017-10-24 11:38:27 +0200

    Create routepublique.tf

---
+++ routepublique.tf
@@ -0,0 +1,29 @@
+resource "aws_internet_gateway" "gwBD" {
+  vpc_id = "${aws_vpc.mainBD.id}"
+
+  tags {
+    Name = "mainBD"
+  }
+}
+
+resource "aws_route_table" "BD" {
+  vpc_id = "${aws_vpc.mainBD.id}"
+
+  route {
+    cidr_block = "172.23.0.0/16"
+    gateway_id = "${aws_internet_gateway.gwBD.id}"
+  }
+
+  tags {
+    Name = "mainBD"
+  }
+}
+
+resource "aws_route_table_association" "BD" {
+  subnet_id      = "${aws_subnet.mainBDSUB1.id}"
+  route_table_id = "${aws_route_table.BD.id}"
+}
+resource "aws_route_table_association" "BD" {
+  subnet_id      = "${aws_subnet.mainBDSUB2.id}"
+  route_table_id = "${aws_route_table.BD.id}"
+}
