494f3a59a12014f62bae87d259170830d8644d37
bobox78910 <33033728+bobox78910@users.noreply.github.com>
2017-10-24 16:14:49 +0200

    Update ami.tf

--- ami.tf
+++ ami.tf
@@ -16,9 +16,31 @@ data "aws_ami" "ubuntu" {
 }
 
 
+resource "aws_security_group" "allow_all" {  name        = "allow_all"  description = "Allow all inbound traffic"  vpc_id = “xxxxx “
+  ingress {    
+    from_port   = 22    
+    to_port     = 22    
+    protocol    = "tcp"    
+    cidr_blocks = ["0.0.0.0/0"]  } 
+  => ADD INGRESS FOR PORT 80  
+  egress {    
+    from_port       = 0    
+    to_port         = 0    
+    protocol        = "-1"    
+    cidr_blocks     = ["0.0.0.0/0"]  } }
+
+data "template_file" "BD" {  
+  template = "${file("${path.module}/userdata.tpl")}"
+  vars {    
+    username = "YYYYYYYYYYYYYYYYYYY"
+    }
+}
+
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
+  key_name = "=>Manual import"
+  vpc_security_group_ids = "${aws_security_group.allow_all.id}"
 
   tags {
     Name = "HelloWorld"
