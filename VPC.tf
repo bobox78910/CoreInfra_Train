918b380efbfba79f7b8a1ffc2d9ada522e4bdf0f
bobox78910 <33033728+bobox78910@users.noreply.github.com>
2017-10-24 10:24:14 +0200

    Update VPC.tf

--- VPC.tf
+++ VPC.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "mainBD" {
   cidr_block       = "172.23.0.0/16"
   instance_tenancy = "dedicated"
 
   tags {
-    Name = "main"
+    Name = "mainBD"
   }
 }
