756189aadb2cf71c4f1ed1455041bc30698842e4
bobox78910 <33033728+bobox78910@users.noreply.github.com>
2017-10-25 09:36:56 +0200

    Update mainbd.tf

--- mainbd.tf
+++ mainbd.tf
@@ -2,3 +2,12 @@
 provider "aws" {
  region     = "eu-west-1"
   }
+resource "aws_vpc" "mainBD" {
+  cidr_block       = "172.23.0.0/16"
+  #instance_tenancy = "dedicated"
+
+  tags {
+    Name = "mainBD"
+  }
+}
+
