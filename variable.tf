9bd6ced71bdc6e52d3bb57f8475ee482b83e5985
bobox78910 <33033728+bobox78910@users.noreply.github.com>
2017-10-25 11:12:10 +0200

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "regionBD" {
+  type = "string"
+  default = "eu-west-1"
+}
+
+variable "BlockBD" {
+  type = "map"
+
+  default = {
+    mainbloc = "172.23.0.0/16"
+    blocsub1 = "172.23.0.0/24"
+    blocksub2 = "172.23.1.0/24"
+  }
+}
+
+variable "zones" {
+  default = ["eu-west-1a", "eu-west-1b"]
+}
