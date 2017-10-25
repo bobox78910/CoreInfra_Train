ad382be28e6a52633d118c6a677d209602194d24
bobox78910 <33033728+bobox78910@users.noreply.github.com>
2017-10-25 09:38:14 +0200

    Update mainbd.tf

--- mainbd.tf
+++ mainbd.tf
@@ -11,3 +11,24 @@ resource "aws_vpc" "mainBD" {
   }
 }
 
+resource "aws_subnet" "mainBDSUB1" {
+  availability_zone = "eu-west-1a"
+  vpc_id     = "${aws_vpc.mainBD.id}"
+  #map_public_ip_on_launch = true
+  cidr_block = "172.23.0.0/24"
+
+  tags {
+    Name = "MainBDSUB1"
+  }
+}
+
+resource "aws_subnet" "mainBDSUB2" {
+  availability_zone = "eu-west-1b"
+  vpc_id     = "${aws_vpc.mainBD.id}"
+  #map_public_ip_on_launch = true
+  cidr_block = "172.23.1.0/24"
+
+  tags {
+    Name = "MainBDSUB2"
+  }
+}
